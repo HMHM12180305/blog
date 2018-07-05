1d4074c0d63e6c47b93744bfcbdc4a2720bb6b0c
HMHM1218 <1611510198b@kindai.ac.jp>
2018-07-05 19:34:55 +0900

    new staticpages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
+  get 'users/show'
+  get 'static_pages/home'
+  get 'static_pages/help'
+  get 'static_pages/about'
+  root  'static_pages#home'
+  get  'static_pages/home'
+  get  'static_pages/about'
+  get  'static_pages/help'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
