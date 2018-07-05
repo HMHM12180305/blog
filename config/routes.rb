b882656f9236216247964166a9b02f874c65f7fe
HMHM1218 <1611510198b@kindai.ac.jp>
2018-07-05 21:11:58 +0900

    complete User

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
-  get 'users/show'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
   root  'static_pages#home'
   get  'static_pages/home'
   get  'static_pages/about'
   get  'static_pages/help'
-
+  resources :users, only: [:new, :create, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
